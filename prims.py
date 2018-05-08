068eb54f5112b320e2a83ff31c139b0914368675
tr2255 <tr2255@gmail.com>
2018-05-08 14:43:16 -0500

    Update prims.py

--- prims.py
+++ prims.py
@@ -5,28 +5,30 @@ Created on Sat May  5 18:24:32 2018
 @author: Adalberto
 """
 
-from functions import G
+from functions import *
 import functions as P
+from Weighted_Graph import *
 
-V = G.vertex_set()
-
-def Prims(Graph,Initial_Vertex)
+def Prims(filename, Initial_Vertex):
+    
+    G = Weighted_Graph(filename)
     VT = {Initial_Vertex} 
     ET = [] 
     MST = (VT, ET) 
+    V = G.vertex_set()
     
     cost_min_edge=P.cost(G,P.min_incident_edge(G,MST))
     
-    ###PRIMS ALGORITHM###
+   ###PRIMS ALGORITHM
     while MST[0] != V:
         min_edge = P.min_incident_edge(G,MST)
         new_vertex = {min_edge[0], min_edge[1]}
-            
         ET.append(min_edge)
         MST =[new_vertex.union(MST[0]),ET]
     print('MST:',MST, '\n')
        
-    total_cost =0
+    
+    total_cost = 0
     for e in MST[0]:
         total_cost += cost_min_edge
     print ('\n', 'Total cost of MST:', total_cost, '\n')
